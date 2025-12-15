d2ae37799c2c2e2b8f2aa1b24b29db046916c0a3
eddierose42 <eddie.rose@lis.ac.uk>
2025-12-15 19:41:19 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+This repository is for our data science group summative assessment, investigating the question ***What economic indicators best predict happiness globally?***
+
+The repo contains the following:
+- **main.ipynb** - notebook containing our project code and 
+- **dashboard.py** - file for pyshiny dashboard
+- **requirements.txt** - includes 
+- **data/** - folder containing datasets downloaded from theglobaleconomy.com
+- **old_data/** - folder including original data downloaded for the projecy
+- **old_cleaning.ipynb** - original data cleaning notebook
+
+To run the code for this project:
+- download github repo and run 'pip install -r requirements.txt'
+- open **main.ipynb** in a jupyter environment and run all cells
+- to launch pyshiny dashboard, run 'shiny run --reload --launch-browser dashboard.py'
+
+Student ID numbers:
+- 24000103464
+- 24000112264
+- 24000102504
---
+++ dashboard.py
@@ -0,0 +1,81 @@
+from shiny import App, ui, render, reactive
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.formula.api as smf
+import seaborn as sns
+import os
+import scipy.stats as stats
+
+files = os.listdir("data/")
+merged = None
+for filename in files:
+    _df = pd.read_csv("data/"+filename)
+    if merged is None:
+        merged = _df
+    else:
+        merged = pd.merge(merged, _df, on=["Country", "Year", "Code", "ContinentCode"], how="outer")
+df = merged[(merged["Year"] >= 2015) & (merged["Year"] <= 2024)]
+
+rename_dict = {"Happiness Index 0 (unhappy) - 10 (happy)": "happiness",
+               "Gross Domestic Product billions of U.S. dollars": "GDP",
+                "GDP per capita current U.S. dollars": "GDP_per_capita",
+                "GDP per capita Purchasing Power Parity": "GDPPPP",
+                "Human Development Index (0 - 1)": "HDI",
+                "Value added in the services sector as percent of GDP": "Services_value_added_GDP"}
+
+df = df.rename(rename_dict, axis=1)
+econ_indicators = ["GDP", "GDPPPP", "GDP_per_capita", "Percent_urban_population", "Services_value_added_GDP", "HDI"]
+years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+
+
+app_ui = ui.page_sidebar(
+    ui.sidebar(
+        ui.h4("Controls"),
+        ui.input_select("indicator", "Economic indicator", choices=econ_indicators, selected="HDI"),
+        ui.input_select("year", "Year", choices=years, selected=2018),
+    ),
+    ui.card(
+        ui.h4("Graph"),
+        ui.output_plot("plot_data"),
+        style="margin:8px; padding:10px;"
+    ),
+    ui.layout_columns(
+        ui.card(
+            ui.h4("Inferential statistics"),
+            ui.output_text("R2"),
+            ui.output_text("pvalue"),
+            style="margin:8px; padding:10px;"
+        ),
+    ),
+    title="Economic metrics predicting happiness"
+)
+
+def server(input, output, session):
+    @output
+    @render.text
+    def R2():
+        data = df[df["Year"] == int(input.year())]
+        model = smf.ols(f'happiness ~ {input.indicator()}', data=data).fit()
+        return f"R-squared: {model.rsquared.round(3)}"
+
+    @output
+    @render.text
+    def pvalue():
+        data = df[df["Year"] == int(input.year())]
+        model = smf.ols(f'happiness ~ {input.indicator()}', data=data).fit()
+        return f"P-value: {model.pvalues[input.indicator()].round(4)}"
+
+    @output
+    @render.plot
+    def plot_data():
+        data = df[df["Year"] == int(input.year())]
+        fig, ax = plt.subplots(figsize=(6, 4))
+        #plt.scatter(x=input.indicator(), y="happiness", data=data)
+        sns.regplot(x=input.indicator(), y="happiness", data=data)
+        #ax.set_ylabel("Number of lines")
+        #ax.set_xlabel("Status")
+        #fig.tight_layout()
+        return fig
+
+app = App(app_ui, server)
